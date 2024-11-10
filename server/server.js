ddd1f347c6165a6d9b149c24cb63aeee046de8cd
Tiffany Mackay <88056784+tiffanymackay@users.noreply.github.com>
2024-11-09 16:08:30 -0800

    update server

--- server/server.js
+++ server/server.js
@@ -11,9 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-    origin: 'https://micropoetry.netlify.app' 
-  }));
+app.use(cors());
 app.use(express.json());
 
 const limiter = rateLimit({
